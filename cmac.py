bd16e3133969e45efb1df6fd38c2faf8d43438ae
aabdulmecitz <ahmetabdulmecid@gmail.com>
2024-09-21 17:40:03 +0300

    sec commit

--- cmac.py
+++ cmac.py
@@ -1,5 +1,16 @@
 import subprocess
+import optparse
 
-subprocess.call(["ifconfig","eth0", "down"])
-subprocess.call(["ifconfig", "eth0", "hw", "ether", "1z:23:45:53:hd:vs"])
-subprocess.call(["ifconfig","eth0", "down"])
+parse_obj = optparse.OptionParser()
+parse_obj.add_option("-i", "--interface",dest='interface',help='interface for changing')
+parse_obj.add_option("-m","--mac", dest='mac',help='mac to change')
+(inputs, args) = parse_obj.parse_args()
+
+interface = inputs.interface
+mac = inputs.mac
+
+print("function started")
+
+subprocess.call(["ifconfig", interface , "down"])
+subprocess.call(["ifconfig", interface , "hw", "ether", mac])
+subprocess.call(["ifconfig", interface , "up"])
