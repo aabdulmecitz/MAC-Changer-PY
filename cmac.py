b4ed728e036a269acdcb48749ee62dc1cfebd5b3
aabdulmecitz <ahmetabdulmecid@gmail.com>
2024-09-21 16:05:30 +0300

    first commit

--- cmac.py
+++ cmac.py
@@ -1,3 +1,5 @@
 import subprocess
 
-subprocess.call([ifconfig, ])
+subprocess.call(["ifconfig","eth0", "down"])
+subprocess.call(["ifconfig", "eth0", "hw", "ether", "1z:23:45:53:hd:vs"])
+subprocess.call(["ifconfig","eth0", "down"])
